9d16427e892bce2939eceed79fb1afea76684201
bossvaitechpro <boss@vaitechpro.com>
2026-05-07 23:22:59 +0200

    Remove dist from git - let Vercel build

--- dist/_next/static/rs_rePhfydXe1AYX_q-va/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Ffunctions\u002F[slug]","\u002Flaws\u002F[slug]","\u002Ftools\u002F[slug]","\u002Fuse-cases\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
